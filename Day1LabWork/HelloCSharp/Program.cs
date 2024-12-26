d7b19d337aa073f38ee5cde5d7948cd939014578
Omkar Bhise <omkar.bhise74@gmail.com>
2024-12-26 23:39:00 +0530

    Day 1 LabWork

---
+++ Day1LabWork/HelloCSharp/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace HelloCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 100;
+            Console.WriteLine(i);
+            Console.WriteLine("Hello c#");
+            Console.ReadLine();
+        }
+    }
+}
