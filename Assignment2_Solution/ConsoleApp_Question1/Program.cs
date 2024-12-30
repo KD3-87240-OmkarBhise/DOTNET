b3a69150e1304610e01cfcaff25ea6547016249c
Omkar Bhise <omkar.bhise74@gmail.com>
2024-12-30 17:45:22 +0530

    Assignment 2

---
+++ Assignment2_Solution/ConsoleApp_Question1/Program.cs
@@ -0,0 +1,108 @@
+﻿using System.Data;
+
+namespace ConsoleApp_Question1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            student.AcceptDetails();
+            student.PrintDetails();
+            
+        }
+
+        
+    }
+
+    public struct Student
+    {
+        private string name;
+        private bool gender;
+        private int age;
+        private int std;
+        private char div;
+        private double marks;
+
+
+        string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        bool Gender
+        {
+            get { return gender; }
+            set { gender = value; }
+        }
+
+        int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        int Std
+        {
+            get { return std; }
+            set { std = value; }
+        }
+
+        char Div
+        {
+            get { return div; }
+            set { div = value; }
+        }
+
+        double Marks
+        {
+            get { return marks; }
+            set { marks = value; }
+        }
+
+        public Student(string name, bool gender, int age, int std, char div, double marks)
+        {
+            this.name = name;
+            this.gender = gender;
+            this.age = age;
+            this.std = std;
+            this.div = div;
+            this.marks = marks;
+        }
+
+        public void AcceptDetails()
+        {
+            Console.WriteLine("Enter Name: ");
+            this.Name = Console.ReadLine();
+
+            Console.WriteLine("Enter Gender: ");
+            this.Gender = Convert.ToBoolean(Console.ReadLine());
+
+            Console.WriteLine("Enter Age: ");
+            this.Age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Std: ");
+            this.Std = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Div: ");
+            this.Div = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine("Enter Marks: ");
+            this.Marks = Convert.ToDouble(Console.ReadLine());
+        }
+
+        public void PrintDetails()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Students data: ");
+            Console.WriteLine("Name: " + this.Name);
+            Console.WriteLine("Gender: " + (this.gender ? "Male" : "Female"));
+            Console.WriteLine("Age: " + this.Age);
+            Console.WriteLine("Std: " + this.Std);
+            Console.WriteLine("Div: " + this.Div);
+            Console.WriteLine("Marks: " + this.Marks);
+        }
+
+    }
+}
---
+++ Assignment2_Solution/ConsoleApp_Question2/Program.cs
@@ -0,0 +1,55 @@
+﻿using ConsoleApp_Question1;
+
+namespace ConsoleApp_Question2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CreateArray();
+        }
+
+        static void CreateArray()
+        {
+            Console.WriteLine("Enter the no. of students: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            Student[] students = new Student[num];
+            AcceptInfo(students);
+        }
+
+        static void AcceptInfo(Student[] students)
+        {
+          
+            for (int i = 0; i < students.Length; i++)
+            {
+                students[i].AcceptDetails();
+            }
+            Console.WriteLine();
+            PrintInfo(students);
+        }
+
+        static void PrintInfo(Student[] students)
+        {
+            for (int i = 0; i < students.Length; i++)
+            {
+                students[i].PrintDetails();
+            }
+            ReverseArray(students);
+        }
+
+        static void ReverseArray(Student[] students)
+        {
+            for (int i = students.Length - 1; i >= 0; i--)
+            {
+                students[i].PrintDetails();
+                
+            }
+
+        }
+
+   
+
+    }
+}
